20aeb035a9107f037b40bbe56750a730c74f1ffb
YuvalAvra <yuval.avrahami@gmail.com>
2021-11-30 08:12:04 -0800

    fix type arm -> arm64

--- util/build_arm64.sh
+++ util/build_arm64.sh
@@ -2,4 +2,4 @@
 set -e
 echo "[+] Building whoc:arm64"
 docker buildx build --build-arg "PLATFORM_LD_PATH_ARG=/lib/ld-linux-aarch64.so.1" \
-  --platform linux/arm64 -f Dockerfile_dynamic -t whoc:arm src
+  --platform linux/arm64 -f Dockerfile_dynamic -t whoc:arm64 src
